7be7ed87e9db8db94e97400d9e81e34d5ece9f92
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-02-24 23:42:01 -0600

    Added list types

---
+++ 02 list types/index.ts
@@ -0,0 +1,7 @@
+const ThingsILove:string[] = ["Music", "Movies", "Skateboarding", "Poetry", "Universe"]
+const LuckyNumbers:number[] = [21, 2, 14, 11]
+const SongsILove:[string, string, number] = ["Bill Evans - Minha", "Jhon Coltrane - I Wish I Knew", 36500]
+
+console.log(`The things I love in the life are: ${ThingsILove.join(", ")}`)
+console.log(`My lucky numbers are: ${LuckyNumbers.join(", ")}`)
+console.log(`And the songs I love are: ${SongsILove.join(", ")}`)
